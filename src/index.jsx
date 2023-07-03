890175871fa3c95b0756a502a0e33bdb30f111db
FujiHai <liufushihai@gmail.com>
2023-07-03 20:39:56 +0800

    feat: add msw(Mock Service Worker)

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// init msw
+if (process.env.NODE_ENV === 'development') {
+  // eslint-disable-next-line global-require
+  const { worker } = require('./mocks/browser')
+  worker.start()
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
