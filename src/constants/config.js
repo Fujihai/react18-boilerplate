17fc74a5e7ba38272e59b73dc0937142ad2425b9
FujiHai <liufushihai@gmail.com>
2023-07-06 11:00:19 +0800

    feat: add mock mode

---
+++ src/constants/config.js
@@ -0,0 +1,4 @@
+export const CONFIG = {
+	apiUrl: process.env.REACT_APP_API_URL ?? '',
+	enableMSW: process.env.REACT_APP_ENABLE_MSW === 'true',
+};
--- src/mocks/setupMocks/index.js
+++ src/mocks/setupMocks/index.js
@@ -1,5 +1,7 @@
+import { CONFIG } from '../../constants/config'
+
 export function setupMocks () {
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === 'development' && CONFIG.enableMSW) {
     // eslint-disable-next-line global-require
     const { worker } = require('./browser')
     worker.start()
